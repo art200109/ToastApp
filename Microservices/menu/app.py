8e93f9f907d1bad1a3a8d2352fe919a7199a0aa1
art200109 <66074399+art200109@users.noreply.github.com>
2022-10-22 23:12:28 +0300

    Update app.py

--- Microservices/menu/app.py
+++ Microservices/menu/app.py
@@ -13,8 +13,8 @@ app = Flask(__name__)
      
 @app.route("/menu")
 def menu():
-    return "abcdefg"
-    #return json.loads(json_util.dumps(client.toast.menu.find()))
+    #return "abcdefg"
+    return json.loads(json_util.dumps(client.toast.menu.find()))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
