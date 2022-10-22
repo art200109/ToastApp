169da3284beaad6a63f95080c44c3aeff01c276f
art200109 <66074399+art200109@users.noreply.github.com>
2022-10-22 21:28:39 +0300

    remove mongo for ocp test

--- Microservices/menu/app.py
+++ Microservices/menu/app.py
@@ -6,13 +6,14 @@ from bson import json_util, ObjectId
 import json
 
 client = MongoClient()
-client = MongoClient('localhost', 27017)
+#client = MongoClient('localhost', 27017)
 
 app = Flask(__name__)
      
 @app.route("/menu")
 def menu():
-    return json.loads(json_util.dumps(client.toast.menu.find()))
+    return "abcdefg"
+    #return json.loads(json_util.dumps(client.toast.menu.find()))
 
 if __name__ == "__main__":
     app.run()
