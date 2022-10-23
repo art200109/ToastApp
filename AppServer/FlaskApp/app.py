7bf2d061537f5720baa6fc2d21488d50aec47481
art200109 <66074399+art200109@users.noreply.github.com>
2022-10-23 03:28:41 +0300

    Update app.py

--- AppServer/FlaskApp/app.py
+++ AppServer/FlaskApp/app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, request
 import os
 import urllib.request, json 
 
-ocp_url = "http://localhost:5000"
+ocp_url = "http://toastapp-myproject.192.168.99.103.nip.io"
 
 template_dir = os.path.dirname(__file__)
 app = Flask(__name__, template_folder=template_dir)
 
-@app.route("/")
+@app.route("/home")
 def home():
     with urllib.request.urlopen(ocp_url+"/menu") as url:
         foods = json.load(url)
     return render_template("index.html",foods = foods)
-      
+
+# Route for handling the login page logic
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+    
 if __name__ == "__main__":
     app.run()
