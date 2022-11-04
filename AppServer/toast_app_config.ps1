b56a3155a72dce79ed6c258ae3cf8ae56b9437f7
Artume Kolesnikov <Artuem@New.Comp>
2022-11-04 22:27:58 +0200

    powershell_edit

--- AppServer/toast_app_config.ps1
+++ AppServer/toast_app_config.ps1
@@ -15,8 +15,42 @@ C:\Python\python.exe -m pip install flask
 C:\Python\python.exe -m pip install wfastcgi --use-pep517
 C:\Python\python.exe -m pip install pymongo
 
-Enable-WindowsOptionalFeature -FeatureName IIS-WebServerRole -Online
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpErrors
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment
+
+Enable-WindowsOptionalFeature -online -FeatureName NetFx4Extended-ASPNET45
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HealthAndDiagnostics
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpLogging
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestMonitor
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpTracing
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Security
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestFiltering
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Performance
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerManagementTools
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-IIS6ManagementCompatibility
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-Metabase
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementConsole
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-BasicAuthentication
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-StaticContent
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-DefaultDocument
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIFilter
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic
+
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
+
 Enable-WindowsOptionalFeature -FeatureName IIS-CGI -Online
+
+Import-Module WebAdministration
 
 C:\Python\Scripts\wfastcgi-enable.exe
 
@@ -31,7 +65,7 @@ New-WebHandler -Name FlaskHandler -Path * -Verb * -Modules FastCgiModule -Script
 
 $python = "C:\inetpub\wwwroot\ToastApp\AppServer\FlaskApp"
 
-$configPath = "system.webServer/fastCgi/application[@arguments='$($python+"\wfastcgi.py")']/environmentVariables/environmentVariable"
+$configPath = "system.webServer/fastCgi/application[@arguments='$($python+"\wfastcgi.py")']/environmentVariables/PYTHONPATH"
 $config = Get-WebConfiguration $configPath
 if (!$config) {
     Add-WebConfiguration $configPath -Value @{ 'Name' = 'PYTHONPATH'; Value = "$python" }
@@ -42,3 +76,9 @@ ConvertTo-WebApplication -PSPath 'IIS:\Sites\Default Web Site\assets'
 Remove-WebHandler FlaskHandler -PSPath 'IIS:\Sites\Default Web Site\assets'
 
 iisreset
+
+Invoke-WebRequest -Uri "https://github.com/minishift/minishift/releases/download/v1.34.3/minishift-1.34.3-windows-amd64.zip" -OutFile "c:/temp/minishift.zip"
+Expand-Archive C:\temp\minishift.zip -DestinationPath C:\temp\
+
+C:\temp\minishift-1.34.3-windows-amd64\minishift.exe config unset vm-driver virtualbox
+C:\temp\minishift-1.34.3-windows-amd64\minishift.exe start 
