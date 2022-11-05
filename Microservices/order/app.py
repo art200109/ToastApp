f8e3f6e41ccda323cf431a965855c08f81adbef6
Artume Kolesnikov <Artuem@New.Comp>
2022-11-05 19:56:21 +0200

    fix int convert

--- Microservices/order/app.py
+++ Microservices/order/app.py
@@ -30,7 +30,7 @@ def order():
         inventory = json.load(url)
         
     for key in meal['recipe']:
-        inventory[key]['amount'] -= meal['recipe'][key]
+        int(inventory[key]['amount']) -= int(meal['recipe'][key])
         if(inventory[key]['amount'] < 0):
             return 'not enough', 422
         requests.put(inventory_url, json=inventory['key'])
