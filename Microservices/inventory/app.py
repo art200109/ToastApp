577ff5bbd198c73d6b7e19dc9973fd1e860a89d3
Artume Kolesnikov <Artuem@New.Comp>
2022-11-05 19:20:16 +0200

    develop inventory feature

---
+++ Microservices/inventory/app.py
@@ -0,0 +1,19 @@
+#sall
+from flask import Flask, render_template, request
+import os
+import urllib.request, json 
+from pymongo import MongoClient
+from bson import json_util, ObjectId
+import json
+
+client = MongoClient()
+client = MongoClient('external-mysql-service.toast.svc', 27017)
+
+app = Flask(__name__)
+     
+@app.route("/")
+def inventory():
+    return json_util.dumps(client.toast.inventory.find())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
