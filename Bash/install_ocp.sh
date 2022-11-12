8e454991c7ead138b5733d2b75b8357536df79dd
Artume Kolesnikov <Artuem@New.Comp>
2022-11-12 17:37:30 +0200

    test

--- Bash/install_ocp.sh
+++ Bash/install_ocp.sh
@@ -63,5 +63,6 @@ PATH=$PATH:/opt/ocp/
 systemctl enable openshift
 systemctl start openshift
 
+#test
 #Edit the file: openshift.local.clusterup/node/node-config.yml and set dnsIP: "" to 8.8.8.8
 #Edit the file openshift.local.clusterup/kubedns/resolv.conf and add
