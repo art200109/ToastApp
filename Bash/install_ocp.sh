e7c8456ef015ae2c658f465e4bc9adb68a74f577
Artume Kolesnikov <Artuem@New.Comp>
2022-11-12 17:35:35 +0200

    delete keys

--- Bash/install_ocp.sh
+++ Bash/install_ocp.sh
@@ -34,9 +34,34 @@ systemctl stop firewalld
 
 mkdir /opt/ocp
 wget -c https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz -O - | sudo tar -xvz -C /opt/ocp/ --strip-components=1
+
+cat << EOF >/etc/systemd/system/openshift.service
+[Unit]
+Description=OpenShift oc cluster up Service
+After=docker.service
+Requires=docker.service
+
+[Service]
+ExecStart=/opt/ocp/oc cluster up --public-hostname=toast-ocp.westeurope.cloudapp.azure.com --routing-suffix=20.13.17.93.nip.io --loglevel=1
+ExecStop=/opt/ocp/oc cluster down
+WorkingDirectory=/var/lib/openshift
+Restart=no
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=occlusterup
+User=root
+Type=forking
+RemainAfterExit=yes
+TimeoutSec=300
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 PATH=$PATH:/opt/ocp/
 
-oc cluster up --public-hostname=toast-ocp.westeurope.cloudapp.azure.com --routing-suffix=20.13.17.93.nip.io
+systemctl enable openshift
+systemctl start openshift
 
 #Edit the file: openshift.local.clusterup/node/node-config.yml and set dnsIP: "" to 8.8.8.8
 #Edit the file openshift.local.clusterup/kubedns/resolv.conf and add
