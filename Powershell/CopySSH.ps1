0c4cf8acbe0e41152e7386a2bbeed67259d5002d
Artume Kolesnikov <Artuem@New.Comp>
2022-11-12 17:04:09 +0200

    delete keys

--- Powershell/CopySSH.ps1
+++ Powershell/CopySSH.ps1
@@ -1,2 +1,7 @@
-﻿ssh-keygen.exe
-type $env:USERPROFILE\.ssh\id_rsa.pub | ssh -i "C:\Users\artum\Downloads\rest.pem"  -p 222 minirest.westeurope.cloudapp.azure.com -l azureuser "cat >> .ssh/authorized_keys"
+﻿$keys = Get-ChildItem $PSScriptRoot\keys
+$private_key_path = "$HOME/.ssh/id_rsa"
+if(-not $(Test-path $private_key_path)) { ssh-keygen.exe -b 2048 -t rsa -f $private_key_path -q -N '""' }
+foreach($key in $keys)
+{
+    type "$private_key_path.pub" | ssh -i $key.fullname  -p 222 $key.name.replace(".pem","") -l azureuser "cat >> .ssh/authorized_keys"
+}
