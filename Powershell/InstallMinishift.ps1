236ba349aec1af72ffced1620376ef78ad202403
Artume Kolesnikov <Artuem@New.Comp>
2022-11-12 15:32:13 +0200

    edit html and scripts

---
+++ Powershell/InstallMinishift.ps1
@@ -0,0 +1,5 @@
+﻿Invoke-WebRequest -Uri "https://github.com/minishift/minishift/releases/download/v1.34.3/minishift-1.34.3-windows-amd64.zip" -OutFile "c:/temp/minishift.zip"
+Expand-Archive C:\temp\minishift.zip -DestinationPath C:\temp\
+
+C:\temp\minishift-1.34.3-windows-amd64\minishift.exe config unset vm-driver virtualbox
+C:\temp\minishift-1.34.3-windows-amd64\minishift.exe start 
