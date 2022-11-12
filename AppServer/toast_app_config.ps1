909d6dc1910d496337fd8e2f01968c3c5e012b21
Artume Kolesnikov <Artuem@New.Comp>
2022-11-12 13:54:35 +0200

    edit scripts

--- AppServer/toast_app_config.ps1
+++ AppServer/toast_app_config.ps1
@@ -7,7 +7,7 @@ if (-not (Test-Path C:\temp))
 
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe" -OutFile "c:/temp/python-3.7.0.exe"
 
-c:/temp/python-3.7.0.exe /q InstallAllUsers=1 PrependPath=1 TargetDir="C:\Python"
+Start-Process -FilePath c:/temp/python-3.7.0.exe -ArgumentList '/q InstallAllUsers=1 PrependPath=1 TargetDir="C:\Python"' -NoNewWindow -Wait
 
 C:\Python\python.exe -m pip install --upgrade pip
 
@@ -22,10 +22,8 @@ Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpErrors
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment
-
 Enable-WindowsOptionalFeature -online -FeatureName NetFx4Extended-ASPNET45
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
-
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HealthAndDiagnostics
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpLogging
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries
@@ -46,10 +44,12 @@ Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIFilter
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic
-
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
+Enable-WindowsOptionalFeature -Online -FeatureName IIS-CGI
 
-Enable-WindowsOptionalFeature -FeatureName IIS-CGI -Online
+
+cd C:\inetpub\wwwroot
+git clone https://github.com/art200109/ToastApp.git
 
 Import-Module WebAdministration
 
@@ -66,20 +66,13 @@ New-WebHandler -Name FlaskHandler -Path * -Verb * -Modules FastCgiModule -Script
 
 $python = "C:\inetpub\wwwroot\ToastApp\AppServer\FlaskApp"
 
-$configPath = "system.webServer/fastCgi/application[@arguments='$($python+"\wfastcgi.py")']/environmentVariables/PYTHONPATH"
-$config = Get-WebConfiguration $configPath
-if (!$config) {
-    Add-WebConfiguration $configPath -Value @{ 'Name' = 'PYTHONPATH'; Value = "$python" }
-    Add-WebConfiguration $configPath -Value @{ 'Name' = 'WSGI_HANDLER'; Value = "app.app" }
-}
+$configPath = "/system.webServer/fastCgi/application[@fullPath='C:\Python\python.exe']" 
+Set-WebConfigurationProperty $configPath -Name arguments -Value $python\wfastcgi.py
+
+Add-WebConfiguration $configPath/environmentVariables -Value @{ 'Name' = 'PYTHONPATH'; Value = "$python" }
+Add-WebConfiguration $configPath/environmentVariables -Value @{ 'Name' = 'WSGI_HANDLER'; Value = "app.app" }
 
 ConvertTo-WebApplication -PSPath 'IIS:\Sites\Default Web Site\assets'
 Remove-WebHandler FlaskHandler -PSPath 'IIS:\Sites\Default Web Site\assets'
 
 iisreset
-
-Invoke-WebRequest -Uri "https://github.com/minishift/minishift/releases/download/v1.34.3/minishift-1.34.3-windows-amd64.zip" -OutFile "c:/temp/minishift.zip"
-Expand-Archive C:\temp\minishift.zip -DestinationPath C:\temp\
-
-C:\temp\minishift-1.34.3-windows-amd64\minishift.exe config unset vm-driver virtualbox
-C:\temp\minishift-1.34.3-windows-amd64\minishift.exe start 
