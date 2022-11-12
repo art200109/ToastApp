c089b3f7be46f4ce88a4f555d78b0810a484060f
Artume Kolesnikov <Artuem@New.Comp>
2022-11-12 19:56:47 +0200

    reorganize

--- Powershell/CopySSH.ps1
+++
@@ -1,7 +0,0 @@
-﻿$keys = Get-ChildItem $PSScriptRoot\keys
-$private_key_path = "$HOME/.ssh/id_rsa"
-if(-not $(Test-path $private_key_path)) { ssh-keygen.exe -b 2048 -t rsa -f $private_key_path -q -N '""' }
-foreach($key in $keys)
-{
-    type "$private_key_path.pub" | ssh -i $key.fullname  -p 222 $key.name.replace(".pem","") -l azureuser "cat >> .ssh/authorized_keys"
-}
