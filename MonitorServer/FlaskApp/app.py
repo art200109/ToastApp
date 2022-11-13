b1dd480ef97dbd817d1293802dd884795a4819b8
Artume Kolesnikov <Artuem@New.Comp>
2022-11-13 09:53:24 +0200

    webhook test

--- MonitorServer/FlaskApp/app.py
+++ MonitorServer/FlaskApp/app.py
@@ -1,1 +1,12 @@
-#To be continued
+from flask import Flask, render_template, redirect, url_for, request
+
+template_dir = os.path.dirname(__file__)
+app = Flask(__name__, template_folder=template_dir)
+
+# Route for handling the login page logic
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    return "Eyal The King"
+       
+if __name__ == "__main__":
+    app.run(debug=True)
