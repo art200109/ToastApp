e3230307058e5a3cc9da6e73d48f315e63f9c06b
Artume Kolesnikov <Artuem@New.Comp>
2022-11-13 09:36:23 +0200

    fix fw

--- General_Scripts/ChangeRDP.ps1
+++ General_Scripts/ChangeRDP.ps1
@@ -3,6 +3,7 @@
     Copy-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Destination "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-222" -Recurse
     Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-222" -Name "PortNumber" -Value 222
     #New-NetFirewallRule -DisplayName "Allow 222 RDP" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 222
-    Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False 
+    #Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+    NetSh Advfirewall set allprofiles state off
     shutdown /r /t 0
 }
