522246d8d6fad6e38922a6998c51c6c820ec6dad
Artume Kolesnikov <Artuem@New.Comp>
2022-11-25 17:17:06 +0200

    pip install pywin32

--- AppServer/FlaskApp/app.py
+++ AppServer/FlaskApp/app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template, redirect, url_for, request
+import json
 import os
-import sys
-import urllib.request, json
-import requests
-from datetime import datetime
-import subprocess
 import socket
+import subprocess
+import sys
+import urllib.request
+from datetime import datetime
+import win32evtlogutil
+import win32evtlog
+
+
+import requests
+from flask import Flask, redirect, render_template, request, url_for
+
 
 def create_minishift_url(service):
     return "http://"+service+"-toast."+minishift_ip+".nip.io"
+
+
+def log():
+    DUMMY_EVT_APP_NAME = "Toast App"
+    DUMMY_EVT_ID = 1234
+
+    win32evtlogutil.ReportEvent(
+        DUMMY_EVT_APP_NAME, DUMMY_EVT_ID,
+        eventType=win32evtlog.EVENTLOG_WARNING_TYPE, strings="אחי הכל סבבה")
 
 
 username = ""
@@ -26,21 +41,24 @@ order_url = create_minishift_url("order")
 template_dir = os.path.dirname(__file__)
 app = Flask(__name__, template_folder=template_dir)
 
+
 @app.route("/home")
 def home():
     with urllib.request.urlopen(menu_url) as url:
         foods = json.load(url)
-    return render_template("index.html",foods = foods, username=username)
+    return render_template("index.html", foods=foods, username=username)
 
 # Route for handling the login page logic
+
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     global username
     error = None
-    if request.method == 'POST':  
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
+
         with urllib.request.urlopen(login_url+"/"+username) as url:
             user = json.load(url)
         if user == "null" or user["password"] != password:
@@ -48,14 +66,15 @@ def login():
         else:
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
-    
-    
+
+
 @app.route("/order", methods=['POST'])
 def order():
     data = request.get_json()
     data["username"] = username
     resp = requests.post(order_url, json=data)
     return (resp.content, resp.status_code, resp.headers.items())
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
