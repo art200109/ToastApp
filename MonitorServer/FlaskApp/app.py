31dd28fdaf827cc2cfd6210bf7d5c4c3fb1de77a
Artume Kolesnikov <Artuem@New.Comp>
2022-12-21 17:23:28 +0200

    group by

--- MonitorServer/FlaskApp/app.py
+++ MonitorServer/FlaskApp/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request
 import os
 import sqlite3
+from operator import itemgetter
+from itertools import groupby
 
 template_dir = os.path.dirname(__file__)
 app = Flask(__name__, template_folder=template_dir)
@@ -8,6 +10,7 @@ db_connect = sqlite3.connect("fake_database.db")
 cursor = db_connect.cursor()
 cursor.execute("select * from cpu")
 temp = cursor.fetchall()
+temp = groupby(temp, key=itemgetter('server_name'))
 cursor.close()
 db_connect.close()
 
