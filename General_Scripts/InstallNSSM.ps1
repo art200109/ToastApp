9c16d5f9b71edb63ed212a656fb9469546450d28
Artume Kolesnikov <Artuem@New.Comp>
2022-12-22 15:41:47 +0200

    asdasd

--- General_Scripts/InstallNSSM.ps1
+++ General_Scripts/InstallNSSM.ps1
@@ -2,7 +2,7 @@
 
 Invoke-WebRequest -Uri "https://nssm.cc/release/nssm-2.24.zip" -OutFile "c:\temp\nssm.zip"
 Expand-Archive C:\temp\nssm.zip -DestinationPath C:\temp\nssm -Force
-
+#
 function Config-NSSM($serviceName, $appPath)
 {
     C:\temp\nssm\nssm-2.24\win64\nssm.exe install $serviceName "C:\Python\python.exe" "$appPath"
