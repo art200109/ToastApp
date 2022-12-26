c55a84021eaacf2b313787e46225917f456dfa9b
Artume Kolesnikov <Artuem@New.Comp>
2022-12-26 11:39:47 +0200

    pip

--- General_Scripts/InstallAgent.sh
+++ General_Scripts/InstallAgent.sh
@@ -2,7 +2,7 @@ yum install -y python3
 wget https://bootstrap.pypa.io/pip/3.6/get-pip.py
 python3 get-pip.py
 
-/usr/bin/pip3 install -r ../MonitoringAgent/requirements.txt
+/usr/local/bin/pip3 install -r ../MonitoringAgent/requirements.txt
 
 cp ../MonitoringAgent/nix_sblunk_uf.service  /etc/systemd/system/
 
