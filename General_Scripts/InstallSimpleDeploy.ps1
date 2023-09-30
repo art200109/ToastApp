c3183c5df5470fdb456ea05de6ed55ad6aa1a265
Artume Kolesnikov <Artuem@New.Comp>
2023-09-30 11:04:30 +0300

    Make simple deploy local

--- General_Scripts/InstallSimpleDeploy.ps1
+++ General_Scripts/InstallSimpleDeploy.ps1
@@ -1,5 +1,5 @@
-﻿Invoke-WebRequest -Uri "https://github.com/jesperhoy/SimpleDeploy/releases/download/v0.9.1/SimpleDeploy-0.9.1.zip" -OutFile "c:/temp/deploy.zip"
-Expand-Archive C:\temp\deploy.zip -DestinationPath C:\inetpub\wwwroot\SimpleDeploy
+﻿# Invoke-WebRequest -Uri "https://github.com/jesperhoy/SimpleDeploy/releases/download/v0.9.1/SimpleDeploy-0.9.1.zip" -OutFile "c:/temp/deploy.zip"
+# Expand-Archive C:\temp\deploy.zip -DestinationPath C:\inetpub\wwwroot\SimpleDeploy
 New-WebSite -Name "SimpleDeploy" -Port 1234 -PhysicalPath "C:\inetpub\wwwroot\SimpleDeploy"
 Remove-WebHandler FlaskHandler -PSPath 'IIS:\Sites\SimpleDeploy'
 icacls C:\inetpub\wwwroot\SimpleDeploy /grant "NT AUTHORITY\IUSR:(OI)(CI)F" /T
