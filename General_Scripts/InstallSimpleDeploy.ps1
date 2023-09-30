9425294d480a6afb546c16d5f75bcaef6940c3af
Artume Kolesnikov <Artuem@New.Comp>
2023-09-30 11:09:18 +0300

    add copy

--- General_Scripts/InstallSimpleDeploy.ps1
+++ General_Scripts/InstallSimpleDeploy.ps1
@@ -1,5 +1,6 @@
 ﻿# Invoke-WebRequest -Uri "https://github.com/jesperhoy/SimpleDeploy/releases/download/v0.9.1/SimpleDeploy-0.9.1.zip" -OutFile "c:/temp/deploy.zip"
 # Expand-Archive C:\temp\deploy.zip -DestinationPath C:\inetpub\wwwroot\SimpleDeploy
+Copy-Item C:\temp\ToastApp\SimpleDeploy C:\inetpub\wwwroot -Force -Recurse
 New-WebSite -Name "SimpleDeploy" -Port 1234 -PhysicalPath "C:\inetpub\wwwroot\SimpleDeploy"
 Remove-WebHandler FlaskHandler -PSPath 'IIS:\Sites\SimpleDeploy'
 icacls C:\inetpub\wwwroot\SimpleDeploy /grant "NT AUTHORITY\IUSR:(OI)(CI)F" /T
